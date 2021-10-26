0e83b25e25bb4e4551b27cbbadc89e4ce81c93cc
M4NUK-W <manuelmcw@gmail.com>
2021-10-26 17:13:42 +0200

    indexV2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
   <title>@yield('title') - Usuarios</title>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link href="/tailwind.css" rel="stylesheet">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
   
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,24 +4,87 @@
 
 
 @section('content')
-<table class="table-auto">
-    <thead>
-      <tr>
-        <th>Usuario</th>
-        <th>correo</th>
-      </tr>
-    </thead>
-    <tbody>
-        @foreach ($users as $row)
-        <tr>
-            <td>{{$row->name}}</td>
-            <td>{{$row->email}}</td>
+<!-- This example requires Tailwind CSS v2.0+ -->
+<div class="flex flex-col">
+  <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+      <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+        <table class="min-w-full divide-y divide-gray-200">
+          <thead class="bg-gray-50">
+            <tr>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Nombre
+              </th>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Clase
+              </th>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Tlf
+              </th>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                Estado
+              </th>
+              <th scope="col" class="relative px-6 py-3">
+                Acciones
+              </th>
+            </tr>
+          </thead>
+          <tbody class="bg-white divide-y divide-gray-200">
+            @foreach ($users as $row)
+            <tr>
+              <td class="px-6 py-4 whitespace-nowrap">
+                <div class="flex items-center">
+                  <div class="flex-shrink-0 h-10 w-10">
+                    <img class="h-10 w-10 rounded-full" src="https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png" alt="">
+                  </div>
+                  <div class="ml-4">
+                    <div class="text-sm font-medium text-gray-900">
+                      {{$row->name}}
+                    </div>
+                    <div class="text-sm text-gray-500">
+                      {{$row->email}}
+                    </div>
+                  </div>
+                </div>
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap">
+                <div class="text-sm text-gray-900">
+                  {{$row->class}}
+                </div>
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap">
+                @if ($row->banned)
+                  <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                    Habilitado
+                  </span>
+                
+                    
+                @else
 
-        </tr>
-        @endforeach
-        
-    </tbody>
-  </table>
+                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
+                  Baneado
+                </span>
+                    
+                @endif
+                
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {{$row->phone}}
+              </td>
+              <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                <a href="#" class="text-red-600 hover:text-red-900">Borrar</a>
+                <a href="#" class="text-indigo-600 hover:text-indigo-900">Editar</a>
+              </td>
+            </tr>
+            @endforeach
+
+            <!-- More people... -->
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
     
 @endsection
     
