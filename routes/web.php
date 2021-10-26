df79057ae30c989d3201ecc5368dd472a0629ba4
Jose <joseluisnicolasnr@gmail.com>
2021-10-26 18:36:32 +0000

    /users not working

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ArticlesController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/food', [ArticlesController::class, 'index']);
+
+Route::get('/users', [UserController::class, 'index']);
+
+Route::get('/users/delete/{id}', [UserController::class , 'destroy'])->name('users.destroy');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
