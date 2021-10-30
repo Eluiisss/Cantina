fb186e5a6fea3a02e97614fb87bf177c1a4ca6b4
eduardonogueraga <eduardo.nogueraga@gmail.com>
2021-10-30 09:55:22 +0000

    Pruebas para borrado logico y definitivo

---
+++ tests/Feature/Admin/DeleteTeamTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use App\Models\Article;
+use App\Models\Category;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class DeleteTeamTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function it_sends_a_article_to_the_trash()
+    {
+        $category = Category::factory()->create(['name' => 'Comida']);
+
+        $article = Article::factory()->create([
+            'category_id' => $category->id
+        ]);
+
+        $articleTrashed = Article::factory()->create([
+            'category_id' => $category->id
+        ]);
+
+        $this->patch(route('articles.trash', ['article' => $articleTrashed]))
+        ->assertRedirect(route('articles.index'));
+
+        $this->assertSoftDeleted('articles',[
+            'id' => $articleTrashed->id,
+        ]);
+
+        $this->assertSoftDeleted('nutrition',[
+            'article_id' => $articleTrashed->id,
+        ]);
+
+        $this->get(route('articles.index'))
+            ->assertOk()
+            ->assertSee($article->name)
+            ->assertDontSee($articleTrashed->name)
+            ->assertViewCollection('articles')
+            ->contains($article)
+            ->notContains($articleTrashed);
+
+        $this->get(route('articles.trashed'))
+            ->assertOk()
+            ->assertViewCollection('articles')
+            ->contains($articleTrashed)
+            ->notContains($article);
+
+    }
+
+    /** @test */
+    function it_completely_deletes_a_article()
+    {
+        $category = Category::factory()->create(['name' => 'Comida']);
+
+        $article = Article::factory()->create([
+            'category_id' => $category->id,
+            'deleted_at'=> now(),
+        ]);
+
+        $this->delete(route('articles.destroy', ['id' => $article->id]))
+            ->assertRedirect(route('articles.trashed'));
+
+        $this->assertDatabaseEmpty('articles');
+        $this->assertDatabaseEmpty('nutrition');
+    }
+
+    /** @test */
+    function it_cannot_delete_a_article_that_is_not_in_the_trash()
+    {
+        $this->withExceptionHandling();
+
+        $category = Category::factory()->create(['name' => 'Comida']);
+
+        $articleNotTrashed = Article::factory()->create([
+            'category_id' => $category->id,
+        ]);
+
+        $this->delete(route('articles.destroy', ['id' => $articleNotTrashed->id]))->assertStatus(404);
+
+        $this->assertDatabaseHas('articles', [
+            'id' => $articleNotTrashed->id,
+            'deleted_at' => null,
+        ]);
+
+        $this->assertDatabaseHas('nutrition', [
+            'article_id' => $articleNotTrashed->id,
+            'deleted_at' => null,
+        ]);
+    }
+}
