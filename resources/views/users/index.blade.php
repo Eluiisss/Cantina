929c2eacc9fa2d76b4b2dc6c8b6140cd8440656f
M4NUK-W <manuelmcw@gmail.com>
2021-11-07 17:09:18 +0100

    sry por el nombre del comit

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -24,7 +24,7 @@
                 Estado
               </th>
               <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Tlf
+                Telefono
               </th>
               <th scope="col" class="relative px-6 py-3">
                 Acciones
