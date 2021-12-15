8a7112469e065a04d8102ca8e2af00866ecf5c04
M4NUK-W <manuelmcw@gmail.com>
2021-12-15 21:26:43 +0100

    test update user

--- tests/Feature/Admin/UsersTest/UpdateUsersTest.php
+++ tests/Feature/Admin/UsersTest/UpdateUsersTest.php
@@ -33,7 +33,7 @@ class UpdateUsersTest extends TestCase
     }
 
     /** @test  */
-    public function it_shows_the_edit_user_page()
+    public function it_shows_the_edit_user_page_to_admins()
     {
         $admin = User::factory()->create([
             'name' => 'User1',
@@ -55,7 +55,7 @@ class UpdateUsersTest extends TestCase
             ->assertSee($user->name);
     }
 
-     /** @test  */
+    /** @test  */
     function an_admin_can__update_a_user()
     {
         $admin = User::factory()->create([
@@ -253,9 +253,209 @@ class UpdateUsersTest extends TestCase
            ]);
        }
 
-     
+       /** @test  */
+       function user_cant_update_credit()
+       {
+           $this->handleValidationExceptions();
+           $this->withExceptionHandling();
+   
+           $userNre = Nre::factory()->create();
+           $oldUser = User::factory()->create([
+               'nre_id' => $userNre->id,
+               'name' => 'Usuario test',
+               'credit' =>5,
+           ]);
+   
+           $userNre->update([
+               'user_id' => $oldUser->id,
+               'updated_at' => now(),
+           ]);
+   
+           $this->assertDatabaseHas('users', [
+                'credit' => 5,
+           ]);
+   
+           $this->actingAs($oldUser)->from(route('users.edit', ['user' => $oldUser]))
+               ->put(route('users.update', ['user' => $oldUser]), $this->withData([
+                   'user_credit' =>10,
+               ]));
+   
+           $this->assertDatabaseHas('users', [
+                'credit' =>5,
+           ]);
+       }
+
+        /** @test  */
+       function employees_can_update_user_credit()
+       {
+            $this->handleValidationExceptions();
+            $this->withExceptionHandling();
+
+            $employee = User::factory()->create([
+            'name' => 'User1',
+            'nre_id' => Nre::factory()->create()->id,
+            ]);
+
+            $employeeRole = Role::create([
+            'name' => 'employee',
+            'display_name' => 'Employee', // optional
+            'description' => 'User can update credit', // optional
+            ]);
+
+            $employee->attachRole($employeeRole);
+   
+            $userNre = Nre::factory()->create();
+            $oldUser = User::factory()->create([
+                'nre_id' => $userNre->id,
+                'name' => 'Usuario test',
+                'credit' =>5,
+            ]);
     
+            $userNre->update([
+                'user_id' => $oldUser->id,
+                'updated_at' => now(),
+            ]);
 
+          
+           $this->assertDatabaseHas('users', [
+                'credit' => 5,
+           ]);
+   
+           $this->actingAs($employee)->from(route('users.edit', ['user' => $oldUser]))
+               ->put(route('users.update', ['user' => $oldUser]), $this->withData([
+                   'user_credit' =>10,
+               ]));
+   
+           $this->assertDatabaseHas('users', [
+                'credit' =>10,
+           ]);
+       }
 
+        /** @test  */
+        function admins_can_update_user_credit()
+        {
+            $this->handleValidationExceptions();
+            $this->withExceptionHandling();
+            $admin = User::factory()->create([
+                'name' => 'User1',
+                'nre_id' => Nre::factory()->create()->id,
+            ]);
+    
+            $adminRole = Role::create([
+                'name' => 'administrator',
+                'display_name' => 'Administrator ', // optional
+                'description' => 'User allowed to see the index of users', // optional
+            ]);
+            $admin->attachRole($adminRole);
+ 
+        
+            $userNre = Nre::factory()->create();
+            $oldUser = User::factory()->create([
+                 'nre_id' => $userNre->id,
+                 'name' => 'Usuario test',
+                 'credit' =>5,
+            ]);
+     
+            $userNre->update([
+                 'user_id' => $oldUser->id,
+                 'updated_at' => now(),
+            ]);
+ 
+           
+            $this->assertDatabaseHas('users', [
+                 'credit' => 5,
+            ]);
+    
+            $this->actingAs($admin)->from(route('users.edit', ['user' => $oldUser]))
+                ->put(route('users.update', ['user' => $oldUser]), $this->withData([
+                    'user_credit' =>10,
+            ]));
+    
+            $this->assertDatabaseHas('users', [
+                 'credit' =>10,
+            ]);
+        }
+
+        /** @test  */
+        public function it_does_not_show_the_credit_field_to_the_user()
+        {
+            $userRole = Role::create([
+                'name' => 'user',
+                'display_name' => 'user', // optional
+                'description' => 'User not allowed to see credit', // optional
+            ]);
+            
+            $userNre = Nre::factory()->create();
+            $user = User::factory()->create([
+                'nre_id' => $userNre->id,
+                'name' => 'Usuario test',
+                'phone' =>'666666666',
+            ]);
+    
+            $userNre->update([
+                'user_id' => $user->id,
+                'updated_at' => now(),
+            ]);
+
+            $user->attachRole($userRole);
+
+            $this->actingAs($user)->get(route('users.edit', ['user' => $user]))
+            ->assertStatus(200)
+            ->assertSee('Editar Usuario')
+            ->assertDontSee('Crédito')
+            ->assertSee($user->name);
+        }
+
+       /** @test  */
+        public function it_shows_the_credit_field_to_admins()
+          {
+              $admin = User::factory()->create([
+                  'name' => 'User1',
+                  'nre_id' => Nre::factory()->create()->id,
+              ]);
+      
+              $adminRole = Role::create([
+                  'name' => 'administrator',
+                  'display_name' => 'Administrator ', // optional
+                  'description' => 'User allowed to see the credit', // optional
+              ]);
+              $admin->attachRole($adminRole);
+      
+              $user = $this->createRandomUser();
+      
+              $this->actingAs($admin)->get(route('users.edit', ['user' => $user]))
+                  ->assertStatus(200)
+                  ->assertSee('Editar Usuario')
+                  ->assertSee('Crédito')
+                  ->assertSee($user->name);
+        }
+
+        /** @test  */
+        public function it_shows_the_user_credit_to_employees()
+        {
+            $employee = User::factory()->create([
+                'name' => 'User1',
+                'nre_id' => Nre::factory()->create()->id,
+            ]);
+    
+            $employeeRole = Role::create([
+                'name' => 'employee',
+                'display_name' => 'Employee', // optional
+                'description' => 'User can update credit', // optional
+            ]);
+    
+            $employee->attachRole($employeeRole);
+       
+
+        $user = $this->createRandomUser();
+
+        $this->actingAs($employee)->get(route('users.edit', ['user' => $user]))
+            ->assertStatus(200)
+            ->assertSee('Editar Usuario')
+            ->assertSee('Crédito')
+            ->assertSee($user->name);
+    }
+       
+    
 
 }
